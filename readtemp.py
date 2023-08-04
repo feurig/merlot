af1e410121a57676b3a719d44aefe6b867a22b09
D Delmar Davis <don@suspectdevices.com>
2023-08-03 20:58:22 -0700

    reading the pep ...

--- readtemp.py
+++ readtemp.py
@@ -10,29 +10,29 @@ import adafruit_ahtx0
 import tempfile
 import os
 
-GotTemperature = False
-GotHumidity = False
-TempRead=0
-HumidityRead=0
+i_can_has_temperature = False
+i_can_has_humidity = False
+temperature_read=0
+humidity_read=0
 
 i2c = board.I2C()  # uses board.SCL and board.SDA
 for tempsensorid in i2c.scan():
     if (tempsensorid==72):
         print ("found a TC74")
         sensor = adafruit_tc74.TC74(i2c)
-        TempRead=sensor.temperature
-        GotTemperature = True
+        temperature_read=sensor.temperature
+        i_can_has_temperature = True
     elif (tempsensorid==56):
         print ("found an AHTx0")
         sensor = adafruit_ahtx0.AHTx0(i2c)
-        TempRead=sensor.temperature
-        GotTemperature = True
-        HumidityRead = sensor.relative_humidity
-        GotHumidity = True
+        temperature_read=sensor.temperature
+        i_can_has_temperature = True
+        humidity_read = sensor.relative_humidity
+        i_can_has_humidity = True
     else:
         print ("found something else ", tempsensorid)
 
-if (GotTemperature):
+if (i_can_has_temperature):
     
     tmpfile =os.path.join(tempfile.gettempdir(),'temp.prom')
 
@@ -45,7 +45,7 @@ if (GotTemperature):
     print("# HELP home_temperature_celsius Temperature read off of external sensor.")
     print("# TYPE home_temperature_celsius gauge")
     print("ambient_temperature_celcius %0.3f" % sensor.temperature)
-    if (GotHumidity):
+    if (i_can_has_humidity):
         print("# HELP home_relative_humidity Temperature read off of external sensor.")
         print("# TYPE home_relative_humidity gauge")
         print("home_relative_humidity %0.3f" % sensor.relative_humidity)
