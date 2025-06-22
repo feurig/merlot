c21f94c284661f462eba5075a28cd476cf017f8d
D Delmar Davis <don@suspectdevices.com>
2025-06-22 12:10:49 -0700

    add temperature

--- bin/timetemp.py
+++ bin/timetemp.py
@@ -18,4 +18,14 @@ display = segments.Seg7x4(i2c)
 display.fill(0)
 while True:
     display.print(time.strftime("%H:%M"))
-    time.sleep(59.9)
+    time.sleep(30)
+    file=open('/var/lib/prometheus/node-exporter/temp.prom')
+    display.colon = False
+    for line in file.readlines():
+        values=line.split()[0:2]
+        if values[0]=='home_temperature_farenheit':
+            display.print(str(values[1])[0:4]+'f')
+    file.close()
+    display.colon = False
+    time.sleep(30)
+    
